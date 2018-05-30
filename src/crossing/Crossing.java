9fef0227a5df3c2dcc780f9c7c1a75f7b92eb208
kuznetsova_a <kuznetsova_a@k-1673.aist.net.ru>
2018-05-30 13:17:50 +0300

    находит пересечение коллекций

---
+++ src/crossing/Crossing.java
@@ -0,0 +1,82 @@
+
+package crossing;
+
+/**
+ *
+ * @author kuznetsova_a
+ */
+
+import java.util.*;
+
+class userdat {
+String UserName, mail, password;
+
+userdat(String UserName_c, String mail_c, String password_c)
+{
+UserName = UserName_c;
+mail = mail_c;
+password = password_c;
+}
+//переопределение метода
+public boolean equals(userdat second)
+    {     if (this.UserName == second.UserName && this.mail == second.mail && this.password== second.password)
+          return true else return false; 
+              }
+
+}
+
+
+public class Crossing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     List<userdat> stringList = new ArrayList<>();//создание нового списка  
+    
+    stringList.add(new userdat ( "Вася","vasyamail.ru","123"));
+    stringList.add(new userdat ( "Антонио","antonio@yandex.ru","666"));
+    stringList.add(new userdat ( "Енот","coon@mail.ru","12345"));
+    stringList.add(new userdat ( "Катя","kat@mail.ru","abc123"));
+    stringList.add(new userdat ( "Жора","jora@mail.ru","qwerty8"));
+    
+    System.out.println("Первый список пользователей");
+    System.out.println(" ");
+    stringList.stream().forEach(s-> System.out.println(s.UserName+ " " + s.mail + " " + s.password));
+    System.out.println(" "); 
+    
+    List<userdat> stringList2 = new ArrayList<>();//создание нового списка  
+    
+    stringList2.add(new userdat ( "Иван","vanomail.ru","1234"));
+    stringList2.add(new userdat ( "Антонио","antonio@yandex.ru","666"));
+    stringList2.add(new userdat ( "Енот","coon@mail.ru","12345"));
+    stringList2.add(new userdat ( "Варвара","varvar@mail.ru","vrvr"));
+    stringList2.add(new userdat ( "Жора","jora@mail.ru","qwerty8"));
+     
+    System.out.println("Второй список пользователей");
+    System.out.println(" ");
+    stringList.stream().forEach(s-> System.out.println(s.UserName+ " " + s.mail + " " + s.password));
+    System.out.println(" ");
+    
+    System.out.println("Пересечение списков пользователей");
+    System.out.println(" ");
+ 
+ 
+    List<userdat> result = new ArrayList<>();
+       
+    
+          for(userdat temp : stringList){
+            for(userdat tmp : stringList2){
+                if(temp.equals(tmp)){
+                    result.add(temp);
+                    
+                }
+            }
+        }
+          
+             result.stream().forEach(s-> System.out.println(s.UserName+ " " + s.mail + " " + s.password));
+
+      
+    }
+    
+}
